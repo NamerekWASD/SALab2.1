2c457a55516c24678d40154553f18bda4e325146
Nikolay <1488nikolay1488@gmail.com>
2022-05-18 17:01:10 +0300

    Normalize usage connection string

--- DAL/Contexts/ApplicationDbContext.cs
+++ DAL/Contexts/ApplicationDbContext.cs
@@ -1,41 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 using Models;
 
 namespace DAL.Contexts
 {
     public class ApplicationDbContext : DbContext
     {
-        private string ConnectionString { get; set; }
         public ApplicationDbContext()
             :base()
         {
-            /*Database.EnsureDeleted();
-            Database.EnsureCreated();*/
         }
-        public ApplicationDbContext(string connectionString)
-            : base()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-            ConnectionString = connectionString;
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
         }
         public DbSet<PlaceModel> Places { get; set; }
         public DbSet<RequestedPlace> RequestedPlaces { get; set; }
         public DbSet<RequestStoreModel> Requests { get; set; }
         public DbSet<FileModel> Files { get; set; }
         public DbSet<CommentModel> Comments { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            optionsBuilder
-                .UseLazyLoadingProxies();
-            if (ConnectionString != null && ConnectionString.Length > 0)
-            {
-                optionsBuilder.UseSqlServer(ConnectionString);
-            }
-            else
-            { 
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PCOdb;Trusted_Connection=True;");
-            }
+            options.UseLazyLoadingProxies()
+                .UseSqlServer("name=ConnectionStrings:DefaultConnection");
         }
     }
 }
